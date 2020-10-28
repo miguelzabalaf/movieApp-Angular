348281345ce8862556bd837da168039424aed5d6
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-10-28 00:12:55 -0500

    add route to redirect home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
